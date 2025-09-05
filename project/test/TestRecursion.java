7a7fc7638b48cf42899f1f2102a46428fb588c62
haido1103 <haido49533@gmail.com>
2025-09-05 17:21:49 +0700

    Create TestRecursion.java
    Signed-off-by: haido1103 <haido49533@gmail.com>

---
+++ project/test/TestRecursion.java
@@ -0,0 +1,7 @@
+import src.Recursion;
+public class TestRecursion {
+    public static void main(String[] args) {
+        System.out.println("5! = " + Recursion.factorial(5));
+        System.out.println("Fibonacci(10) = " + Recursion.fibonacci(10));
+    }
+}
