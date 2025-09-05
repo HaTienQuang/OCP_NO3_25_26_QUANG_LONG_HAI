f1d282dfdd01f866851a099201212cf364550030
haido1103 <haido49533@gmail.com>
2025-09-05 17:28:42 +0700

    Create App.java
    Signed-off-by: haido1103 <haido49533@gmail.com>

---
+++ project/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=== Test User ===");
+        TestUser.main(args);
+
+        System.out.println("\n=== Test Time ===");
+        TestTime.main(args);
+
+        System.out.println("\n=== Test Recursion ===");
+        TestRecursion.main(args);
+    }
+}
