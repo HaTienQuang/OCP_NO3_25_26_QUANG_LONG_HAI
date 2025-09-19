4fc6841c8188c43041c07a3f0d97c7ab217a2188
haido1103 <haido49533@gmail.com>
2025-09-19 16:53:11 +0700

    Delete project/test directory
    Signed-off-by: haido1103 <haido49533@gmail.com>

--- project/test/TestRecursion.java
+++
@@ -1,7 +0,0 @@
-import src.Recursion;
-public class TestRecursion {
-    public static void main(String[] args) {
-        System.out.println("5! = " + Recursion.factorial(5));
-        System.out.println("Fibonacci(10) = " + Recursion.fibonacci(10));
-    }
-}
