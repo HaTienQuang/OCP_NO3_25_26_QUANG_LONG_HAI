cdeabb454990dc56424cba7f831ead30a0e6717d
HaTienQuang <119412057+HaTienQuang@users.noreply.github.com>
2025-11-03 18:31:25 +0700

    Delete main directory
    Signed-off-by: HaTienQuang <119412057+HaTienQuang@users.noreply.github.com>

--- main/java/com/store/quanlysieuthi/Repository/HoaDonChiTietRepository.java
+++
@@ -1,8 +0,0 @@
-package com.store.quanlysieuthi.Repository;
-
-import com.store.quanlysieuthi.Model.HoaDonChiTiet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HoaDonChiTietRepository extends JpaRepository<HoaDonChiTiet, Long> {
-    // Có thể thêm các hàm tìm kiếm chi tiết sau (ví dụ: theo hóa đơn ID)
-}
